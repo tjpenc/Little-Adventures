aa6d9aff0788ff04d3a2112e2f6958d08ddd380c
Thomas Pencarinha <tjpencarinha@gmail.com>
2023-05-27 11:00:47 -0400

    fixed next.js mulitple children components issue

--- components/buttons/AdventuresButton.js
+++ components/buttons/AdventuresButton.js
@@ -4,7 +4,7 @@ import { Button } from 'react-bootstrap';
 export default function DiscoveriesButton() {
   return (
     <Link href="/adventures/personal/myAdventures" passHref>
-      <Button>Adventures</Button>;
+      <Button>Adventures</Button>
     </Link>
   );
 }
--- components/buttons/DiscoveriesButton.js
+++ components/buttons/DiscoveriesButton.js
@@ -4,7 +4,7 @@ import { Button } from 'react-bootstrap';
 export default function DiscoveriesButton() {
   return (
     <Link href="/discoveries/personal/myDiscoveries" passHref>
-      <Button>Discoveries</Button>;
+      <Button>Discoveries</Button>
     </Link>
   );
 }
