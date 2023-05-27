0aa01248e641c7f0c66965086e628b3bfa7fb3c7
Thomas Pencarinha <tjpencarinha@gmail.com>
2023-05-27 10:48:06 -0400

    This is the push with the next.js link error in the adventures button and discoveries button

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,54 @@
+import { Card, Button } from 'react-bootstrap';
+import styled from 'styled-components';
 import { useAuth } from '../utils/context/authContext';
+import DiscoveriesButton from '../components/buttons/DiscoveriesButton';
+import AdventuresButton from '../components/buttons/AdventuresButton';
 
 function Home() {
   const { user } = useAuth();
+  console.warn(user);
 
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Hello {user.displayName}! </h1>
-    </div>
+    <>
+      <NavigationButtonContainers>
+        <Button>Explore Adventures</Button>
+        <Button>Explore Discoveries</Button>
+        <Button>Find an Adventure</Button>
+        <Button>To Be Explored</Button>
+      </NavigationButtonContainers>
+      <div
+        className="text-center d-flex flex-column justify-content-center align-content-center"
+        style={{
+          height: '90vh',
+          padding: '30px',
+          maxWidth: '400px',
+          margin: '0 auto',
+        }}
+      >
+        <h1>Hello {user.displayName}! </h1>
+        <Card.Img
+          style={{
+            height: '50px',
+            width: '50px',
+          }}
+          src={user.photoURL}
+          alt="user"
+        />
+        <FilterButtonContainer>
+          <AdventuresButton />
+          <DiscoveriesButton />
+        </FilterButtonContainer>
+      </div>
+    </>
   );
 }
 
 export default Home;
+
+const NavigationButtonContainers = styled.div`
+
+`;
+
+const FilterButtonContainer = styled.div`
+
+`;
